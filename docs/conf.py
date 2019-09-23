679220458c438c05b5c6a4fe1eb2c96166f2b5d7
Egor Kostan <igorkostan@gmail.com>
2019-09-23 15:05:57 -0700

    Update conf.py
    [C0111(missing-docstring)] Missing module docstring

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,17 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+ Configuration file for the Sphinx documentation builder.
 
-# -- Path setup --------------------------------------------------------------
+ This file only contains a selection of the most common options. For a full
+ list see the documentation:
+ https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+ -- Path setup --------------------------------------------------------------
+
+ If extensions (or modules to document with autodoc) are in another directory,
+ add these directories to sys.path here. If the directory is relative to the
+ documentation root, use os.path.abspath to make it absolute, like shown here.
+"""
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
