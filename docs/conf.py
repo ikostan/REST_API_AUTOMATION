904c8afc21b262064fbb529a08147fced669858d
Egor Kostan <igorkostan@gmail.com>
2019-09-23 15:11:51 -0700

    Update conf.py
    [C0111(missing-docstring)] Missing module docstring

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 """
  Configuration file for the Sphinx documentation builder.
 
@@ -14,6 +16,7 @@
 
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.append(os.path.abspath('../..'))
 sys.path.append(os.path.abspath('..'))
