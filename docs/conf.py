0550b45e199074d32b34c2a597204520d273db1a
Egor Kostan <igorkostan@gmail.com>
2019-09-23 12:45:22 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,13 +24,13 @@ author = 'Egor Kostan'
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+	'sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -40,7 +40,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -53,3 +52,9 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# Tee master toctree document.
+master_doc = 'index'
