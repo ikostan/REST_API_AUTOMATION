6854ce7804f0cdf322c463986f99dc58552dd36a
Egor Kostan <igorkostan@gmail.com>
2019-09-26 13:32:21 -0700

    Create get_args_from_cli.py

---
+++ utils/get_args_from_cli.py
@@ -0,0 +1,27 @@
+"""Extract arguments from CLI"""
+
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+import sys
+
+
+def get_args():
+    """
+    Reads arguments from CLI and returns them as dictionary.
+    Helps running Python unittest with command-line arguments.
+    Raises Exception if one of the required arguments is missing.
+    :return:
+    """
+
+    env = None
+
+    for param in sys.argv:
+
+        # Test environment (mainly for web testing)
+        if '--env=' in param:
+            env = str(param).replace('--env=', '')
+            print("\n--env={}".format(env))
+
+    return {'env': env}
