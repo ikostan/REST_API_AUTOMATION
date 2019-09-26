a2ea8dae54744c295f0d57072ee3d0562fd00a7f
Egor Kostan <igorkostan@gmail.com>
2019-09-25 23:25:36 -0700

    Update get_test.py

--- tests/get_test.py
+++ tests/get_test.py
@@ -9,11 +9,12 @@ import requests
 import cars_app
 
 
-class GetTestCase(unittest.TestCase):
+class GetCarsTestCase(unittest.TestCase):
 
 	@classmethod
 	def setUpClass(cls) -> None:
 		cls.URL = 'http://127.0.0.1:5000/cars'
+
 		cls.CARS_LIST = [{"name": "Swift",
 		                  "brand": "Maruti",
 		                  "price_range": "3-5 lacs",
@@ -31,6 +32,24 @@ class GetTestCase(unittest.TestCase):
 		                  "price_range": "7-10 lacs",
 		                  "car_type": "sedan"}]
 
+		cls.CARS_HATCHBACK = [{"name": "Swift",
+		                       "brand": "Maruti",
+		                       "price_range": "3-5 lacs",
+		                       "car_type": "hatchback"},
+		                      {"name": "Creta",
+		                       "brand": "Hyundai",
+		                       "price_range": "8-14 lacs",
+		                       "car_type": "hatchback"}]
+
+		cls.CARS_SEDAN = [{"name": "City",
+		                   "brand": "Honda",
+		                   "price_range": "3-6 lacs",
+		                   "car_type": "sedan"},
+		                  {"name": "Vento",
+		                   "brand": "Volkswagen",
+		                   "price_range": "7-10 lacs",
+		                   "car_type": "sedan"}]
+
 	def test_get_list_of_cars_admin(self):
 		username = cars_app.user_list[0]['name']
 		password = cars_app.user_list[0]['password']
@@ -41,18 +60,50 @@ class GetTestCase(unittest.TestCase):
 		                                        password))
 		self.assertEqual(200,
 		                 response.status_code)
+
 		self.assertListEqual(self.CARS_LIST,
 		                     response.json()['cars_list'])
 
 	def test_get_list_of_cars_non_admin(self):
-		username = cars_app.user_list[0]['name']
-		password = cars_app.user_list[0]['password']
-		self.assertEqual("non_admin", 
+		username = cars_app.user_list[1]['name']
+		password = cars_app.user_list[1]['password']
+		self.assertEqual("non_admin",
 		                 cars_app.user_list[1]['perm'])
 
 		response = requests.get(self.URL, auth=(username,
 		                                        password))
 		self.assertEqual(200,
 		                 response.status_code)
+
 		self.assertListEqual(self.CARS_LIST,
 		                     response.json()['cars_list'])
+
+	def test_get_list_of_cars_non_admin_sedan(self):
+		username = cars_app.user_list[1]['name']
+		password = cars_app.user_list[1]['password']
+		self.assertEqual("non_admin",
+		                 cars_app.user_list[1]['perm'])
+
+		response = requests.get(self.URL + '/filter/sedan',
+		                        auth=(username, password))
+
+		self.assertEqual(200,
+		                 response.status_code)
+
+		self.assertListEqual(self.CARS_SEDAN,
+		                     response.json()['cars'])
+
+	def test_get_list_of_cars_admin_hatchback(self):
+		username = cars_app.user_list[0]['name']
+		password = cars_app.user_list[0]['password']
+		self.assertEqual("admin",
+		                 cars_app.user_list[0]['perm'])
+
+		response = requests.get(self.URL + '/filter/hatchback',
+		                        auth=(username, password))
+
+		self.assertEqual(200,
+		                 response.status_code)
+
+		self.assertListEqual(self.CARS_HATCHBACK,
+		                     response.json()['cars'])
