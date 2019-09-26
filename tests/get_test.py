09bb01af35ab457c7db9995c070ffb61dd38aa38
Egor Kostan <igorkostan@gmail.com>
2019-09-26 13:18:05 -0700

    Update get_test.py

--- tests/get_test.py
+++ tests/get_test.py
@@ -1,124 +1,191 @@
 #!/path/to/interpreter
 
 """
-Flask REST API testing
+Flask App REST API testing
 """
 
 import unittest
+import allure
 import requests
 import cars_app
 
 
-@allure.epic('ParaBank Web App')
-@allure.parent_suite('End To End')
-@allure.suite("User Login/Logout")
+@allure.epic('Simple Flask App')
+@allure.parent_suite('Integration Tests')
+@allure.suite("REST API")
 @allure.sub_suite("Positive Tests")
-@allure.feature("Admin Page")
-@allure.story('Login/Logout Functionality')
+@allure.feature("GET")
+@allure.story('Cars')
 class GetCarsTestCase(unittest.TestCase):
+	"""
+	Simple Flask App Test: GET call > cars
+	"""
 
 	@classmethod
 	def setUpClass(cls) -> None:
+		"""
+		Test data preparation
+		:return:
+		"""
 
-		cls.URL = 'http://127.0.0.1:5000/cars'
+		with allure.step("Set test URL"):
+			cls.URL = 'http://127.0.0.1:5000/cars'
 
-		cls.CARS_HATCHBACK = [{"name": "Swift",
-		                       "brand": "Maruti",
-		                       "price_range": "3-5 lacs",
-		                       "car_type": "hatchback"},
-		                      {"name": "Creta",
-		                       "brand": "Hyundai",
-		                       "price_range": "8-14 lacs",
-		                       "car_type": "hatchback"}]
+		with allure.step("Arrange expected results (cars list)"):
+			cls.CARS_HATCHBACK = [{"name": "Swift",
+			                       "brand": "Maruti",
+			                       "price_range": "3-5 lacs",
+			                       "car_type": "hatchback"},
+			                      {"name": "Creta",
+			                       "brand": "Hyundai",
+			                       "price_range": "8-14 lacs",
+			                       "car_type": "hatchback"}]
 
-		cls.CARS_SEDAN = [{"name": "City",
-		                   "brand": "Honda",
-		                   "price_range": "3-6 lacs",
-		                   "car_type": "sedan"},
-		                  {"name": "Vento",
-		                   "brand": "Volkswagen",
-		                   "price_range": "7-10 lacs",
-		                   "car_type": "sedan"}]
+			cls.CARS_SEDAN = [{"name": "City",
+			                   "brand": "Honda",
+			                   "price_range": "3-6 lacs",
+			                   "car_type": "sedan"},
+			                  {"name": "Vento",
+			                   "brand": "Volkswagen",
+			                   "price_range": "7-10 lacs",
+			                   "car_type": "sedan"}]
 
-		cls.CARS_LIST = cls.CARS_HATCHBACK + cls.CARS_SEDAN
+			cls.CARS_LIST = cls.CARS_HATCHBACK + cls.CARS_SEDAN
 
 	def test_get_list_of_cars_admin(self):
-		username = cars_app.user_list[0]['name']
-		password = cars_app.user_list[0]['password']
-		self.assertEqual("admin",
-		                 cars_app.user_list[0]['perm'])
+		"""
+		Get full list of cars using admin user credentials.
+		:return:
+		"""
 
-		response = requests.get(self.URL, auth=(username,
-		                                        password))
-		self.assertEqual(200,
-		                 response.status_code)
+		with allure.step("Verify user permissions"):
+			username = cars_app.user_list[0]['name']
+			password = cars_app.user_list[0]['password']
+			self.assertEqual("admin",
+			                 cars_app.user_list[0]['perm'])
 
-		self.assertTrue(response.json()['successful'])
+		with allure.step("Send GET request"):
+			response = requests.get(self.URL, auth=(username,
+			                                        password))
 
-		self.assertListEqual(self.CARS_LIST,
-		                     response.json()['cars_list'])
+		with allure.step("Verify status code"):
+			self.assertEqual(200,
+			                 response.status_code)
+
+		with allure.step("Verify 'successful' flag"):
+			self.assertTrue(response.json()['successful'])
+
+		with allure.step("Verify retrieved cars list"):
+			self.assertListEqual(self.CARS_LIST,
+			                     response.json()['cars_list'])
 
 	def test_get_list_of_cars_non_admin(self):
-		username = cars_app.user_list[1]['name']
-		password = cars_app.user_list[1]['password']
-		self.assertEqual("non_admin",
-		                 cars_app.user_list[1]['perm'])
+		"""
+		Get full list of cars using non admin user credentials.
+		:return:
+		"""
 
-		response = requests.get(self.URL, auth=(username,
-		                                        password))
-		self.assertEqual(200,
-		                 response.status_code)
+		with allure.step("Verify user permissions"):
+			username = cars_app.user_list[1]['name']
+			password = cars_app.user_list[1]['password']
+			self.assertEqual("non_admin",
+			                 cars_app.user_list[1]['perm'])
 
-		self.assertTrue(response.json()['successful'])
+		with allure.step("Send GET request"):
+			response = requests.get(self.URL, auth=(username,
+			                                        password))
 
-		self.assertListEqual(self.CARS_LIST,
-		                     response.json()['cars_list'])
+		with allure.step("Verify status code"):
+			self.assertEqual(200,
+			                 response.status_code)
+
+		with allure.step("Verify 'successful' flag"):
+			self.assertTrue(response.json()['successful'])
+
+		with allure.step("Verify retrieved cars list"):
+			self.assertListEqual(self.CARS_LIST,
+			                     response.json()['cars_list'])
 
 	def test_get_list_of_cars_non_admin_sedan(self):
-		username = cars_app.user_list[1]['name']
-		password = cars_app.user_list[1]['password']
-		self.assertEqual("non_admin",
-		                 cars_app.user_list[1]['perm'])
+		"""
+		Get full list of cars from type = 'sedan'
+		using non admin user credentials.
+		:return:
+		"""
 
-		response = requests.get(self.URL + '/filter/sedan',
-		                        auth=(username, password))
+		with allure.step("Verify user permissions"):
+			username = cars_app.user_list[1]['name']
+			password = cars_app.user_list[1]['password']
+			self.assertEqual("non_admin",
+			                 cars_app.user_list[1]['perm'])
 
-		self.assertEqual(200,
-		                 response.status_code)
+		with allure.step("Send GET request"):
+			response = requests.get(self.URL + '/filter/sedan',
+			                        auth=(username, password))
 
-		self.assertListEqual(self.CARS_SEDAN,
-		                     response.json()['cars'])
+		with allure.step("Verify status code"):
+			self.assertEqual(200,
+			                 response.status_code)
+
+		with allure.step("Verify retrieved cars list"):
+			self.assertListEqual(self.CARS_SEDAN,
+			                     response.json()['cars'])
 
 	def test_get_list_of_cars_admin_hatchback(self):
-		username = cars_app.user_list[0]['name']
-		password = cars_app.user_list[0]['password']
-		self.assertEqual("admin",
-		                 cars_app.user_list[0]['perm'])
+		"""
+		Get full list of cars from type = 'hatchback'
+		using admin user credentials.
+		:return:
+		"""
 
-		response = requests.get(self.URL + '/filter/hatchback',
-		                        auth=(username, password))
+		with allure.step("Verify user permissions"):
+			username = cars_app.user_list[0]['name']
+			password = cars_app.user_list[0]['password']
+			self.assertEqual("admin",
+			                 cars_app.user_list[0]['perm'])
 
-		self.assertListEqual(self.CARS_HATCHBACK,
-		                     response.json()['cars'])
+		with allure.step("Send GET request"):
+			response = requests.get(self.URL + '/filter/hatchback',
+			                        auth=(username, password))
+
+		with allure.step("Verify status code"):
+			self.assertEqual(200,
+			                 response.status_code)
+
+		with allure.step("Verify retrieved cars list"):
+			self.assertListEqual(self.CARS_HATCHBACK,
+			                     response.json()['cars'])
 
 	def test_get_car_by_name_non_admin_swift(self):
-		username = cars_app.user_list[1]['name']
-		password = cars_app.user_list[1]['password']
-		self.assertEqual("non_admin",
-		                 cars_app.user_list[1]['perm'])
+		"""
+		Get car data by name = 'swift'
+		using non admin user credentials.
+		:return:
+		"""
 
-		car = {"brand": "Maruti",
-		       "car_type": "hatchback",
-		       "name": "Swift",
-		       "price_range": "3-5 lacs"}
+		with allure.step("Verify user permissions"):
+			username = cars_app.user_list[1]['name']
+			password = cars_app.user_list[1]['password']
+			self.assertEqual("non_admin",
+			                 cars_app.user_list[1]['perm'])
 
-		response = requests.get(self.URL + '/Swift',
-		                        auth=(username, password))
+		with allure.step("Prepare expected results"):
+			car = {"brand": "Maruti",
+			       "car_type": "hatchback",
+			       "name": "Swift",
+			       "price_range": "3-5 lacs"}
 
-		self.assertEqual(200,
-		                 response.status_code)
+		with allure.step("Send GET request"):
+			response = requests.get(self.URL + '/Swift',
+			                        auth=(username, password))
 
-		self.assertTrue(response.json()['successful'])
+		with allure.step("Verify status code"):
+			self.assertEqual(200,
+			                 response.status_code)
 
-		self.assertDictEqual(car,
-		                     response.json()['car'])
+		with allure.step("Verify 'successful' flag"):
+			self.assertTrue(response.json()['successful'])
+
+		with allure.step("Verify retrieved car data"):
+			self.assertDictEqual(car,
+			                     response.json()['car'])
