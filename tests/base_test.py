5996c9ba651820e5ce3e454ac88b04286d053d1e
Egor Kostan <igorkostan@gmail.com>
2019-09-30 22:20:05 -0700

    Update base_test.py

--- tests/base_test.py
+++ tests/base_test.py
@@ -43,7 +43,7 @@ class BaseTestCase(unittest.TestCase):
                 os.system("start /B start cmd.exe @cmd /k python ../cars_app.py")
 
             if platform.system() == 'Linux':
-                os.system("python ../cars_app.py &")
+                # os.system("python ../cars_app.py &")
                 # pass
 
         time.sleep(5)
@@ -62,5 +62,5 @@ class BaseTestCase(unittest.TestCase):
                 pass
 
             if platform.system() == 'Linux':
-                os.system('pkill -f cars_app.py')
+                # os.system('pkill -f cars_app.py')
                 # pass
