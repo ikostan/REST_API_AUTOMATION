7e795466612516e546473e5339d9b2b971c343ea
Egor Kostan <igorkostan@gmail.com>
2019-10-02 21:14:10 -0700

    Create internal_func_positive_test.py

---
+++ tests/unit/internal_func_positive_test.py
@@ -0,0 +1,76 @@
+#!/path/to/interpreter
+
+"""
+Flask App REST API testing: Unit Tests
+"""
+
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+import allure
+import unittest
+from api.cars_app import app, check_auth, authenticate_error
+
+
+@allure.epic('Simple Flask App')
+@allure.parent_suite('REST API')
+@allure.suite("Unit Tests")
+@allure.sub_suite("Positive Tests")
+@allure.feature("Internal Functions")
+@allure.story('Flags and Args Validation')
+class InternalFuncPositiveTestCase(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls) -> None:
+
+		with allure.step("Arrange test data"):
+			cls.admin_user = {"name": "qxf2",
+			                  "password": "qxf2",
+			                  "perm": "admin"}
+
+			cls.non_admin_user = {"name": "jack",
+			                      "password": "qxf2",
+			                      "perm": "non_admin"}
+
+	def test_check_auth_admin(self):
+
+		with allure.step("Verify check_auth flag using admin user"):
+			self.assertTrue(check_auth(username=self.admin_user["name"],
+			                           password=self.admin_user["password"]))
+
+	def test_check_auth_non_admin(self):
+
+		with allure.step("Verify check_auth flag using non admin user"):
+			self.assertTrue(check_auth(username=self.non_admin_user["name"],
+			                           password=self.non_admin_user["password"]))
+
+	def test_authenticate_error_flag_true(self):
+
+		with app.app_context():
+
+			with allure.step("Arrange authenticate_error response"):
+				response = authenticate_error(auth_flag=True)
+
+			with allure.step("Verify status code"):
+				self.assertEqual(401,
+				                 response.status_code)
+
+			with allure.step("Verify header"):
+				self.assertEqual('Basic realm="Example"',
+				                 response.headers['WWW-Authenticate'])
+
+	def test_authenticate_error_flag_false(self):
+
+		with app.app_context():
+
+			with allure.step("Arrange authenticate_error response"):
+				response = authenticate_error(auth_flag=False)
+
+			with allure.step("Verify status code"):
+				self.assertEqual(401,
+				                 response.status_code)
+
+			with allure.step("Verify header"):
+				self.assertEqual('Basic realm="Example"',
+				                 response.headers['WWW-Authenticate'])
