2531100cfb64d86e65dfb1d038ad3dee9a970eaf
Egor Kostan <igorkostan@gmail.com>
2019-10-03 19:06:07 -0700

    Update put_call_test.py

--- tests/integration/put_call_test.py
+++ tests/integration/put_call_test.py
@@ -15,44 +15,45 @@ from api.cars_app import app
 
 
 class PutCallTestCase(unittest.TestCase):
-	"""
-	Testing a JSON API implemented in Flask.
-	PUT Call Integration Test.
-	"""
+    """
+    Testing a JSON API implemented in Flask.
+    PUT Call Integration Test.
+    """
 
-	def test_debug_mode(self):
-		self.assertFalse(app.config['DEBUG'])
+    def test_debug_mode(self):
+        self.assertFalse(app.config['DEBUG'])
 
-	def test_put_cars_update(self):
-		car = {"name": "Creta",
-		       "brand": "Hyundai",
-		       "price_range": "8-14 lacs",
-		       "car_type": "hatchback"}
+    def test_put_cars_update(self):
+        car = {"name": "Creta",
+               "brand": "Hyundai",
+               "price_range": "8-14 lacs",
+               "car_type": "hatchback"}
 
-		user = {"name": "eric",
-		        "password": "testqxf2",
-		        "perm": "non_admin"}
+        user = {"name": "eric",
+                "password": "testqxf2",
+                "perm": "non_admin"}
 
-		response = app.test_client().put(
-			'{}{}'.format('/cars/update/',
-			              car['name']),
-			data=json.dumps(car),
-			content_type='application/json',
+        response = app.test_client().put(
+            '{}{}'.format('/cars/update/',
+                          car['name']),
+            data=json.dumps(car),
+            content_type='application/json',
 
-			# Testing Flask application
-			# with basic authentication
-			# Source: https://gist.github.com/jarus/1160696
-			headers={
-				'Authorization': 'Basic ' + base64.b64encode(bytes(user['name'] +
-				                                                   ":" +
-				                                                   user['password'],
-				                                                   'ascii')).decode('ascii')
-			}
-		)
+            # Testing Flask application
+            # with basic authentication
+            # Source: https://gist.github.com/jarus/1160696
+            headers={
+                'Authorization': 'Basic ' +
+                                 base64.b64encode(bytes(user['name'] +
+                                                        ":" +
+                                                        user['password'],
+                                                        'ascii')).decode('ascii')
+            }
+        )
 
-		self.assertEqual(200, response.status_code)
+        self.assertEqual(200, response.status_code)
 
-		data = json.loads(response.get_data(as_text=True))
-		print("\nDATA:\n{}\n".format(data))  # Debug only
+        data = json.loads(response.get_data(as_text=True))
+        print("\nDATA:\n{}\n".format(data))  # Debug only
 
-		self.assertTrue(data['response']['successful'])
+        self.assertTrue(data['response']['successful'])
