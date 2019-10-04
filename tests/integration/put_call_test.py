22dbc3bf99d9cb1315e60d9c131f0de6a7922aab
Egor Kostan <igorkostan@gmail.com>
2019-10-03 19:25:29 -0700

    Update put_call_test.py

--- tests/integration/put_call_test.py
+++ tests/integration/put_call_test.py
@@ -10,50 +10,89 @@ PUT Call Integration Test
 
 import base64
 import unittest
+
+import allure
 from flask import json
 from api.cars_app import app
 
 
 class PutCallTestCase(unittest.TestCase):
-    """
+	"""
     Testing a JSON API implemented in Flask.
     PUT Call Integration Test.
     """
 
-    def test_debug_mode(self):
-        self.assertFalse(app.config['DEBUG'])
+	def test_debug_mode(self):
+		"""
+		Whether debug mode is enabled.
 
-    def test_put_cars_update(self):
-        car = {"name": "Creta",
-               "brand": "Hyundai",
-               "price_range": "8-14 lacs",
-               "car_type": "hatchback"}
+		When using flask run to start the development server,
+		an interactive debugger will be shown for unhandled exceptions,
+		and the server will be reloaded when code changes.
+		The debug attribute maps to this config key. This is enabled
+		when ENV is 'development' and is overridden by the FLASK_DEBUG
+		environment variable. It may not behave as expected if set in code.
 
-        user = {"name": "eric",
-                "password": "testqxf2",
-                "perm": "non_admin"}
+		Do not enable debug mode when deploying in production.
 
-        response = app.test_client().put(
-            '{}{}'.format('/cars/update/',
-                          car['name']),
-            data=json.dumps(car),
-            content_type='application/json',
+		Default: True if ENV is 'development', or False otherwise.
+		:return:
+		"""
 
-            # Testing Flask application
-            # with basic authentication
-            # Source: https://gist.github.com/jarus/1160696
-            headers={
-                'Authorization': 'Basic ' +
-                                 base64.b64encode(bytes(user['name'] +
-                                                        ":" +
-                                                        user['password'],
-                                                        'ascii')).decode('ascii')
-            }
-        )
+		allure.dynamic.title("API flags validation")
+		allure.dynamic.severity(allure.severity_level.CRITICAL)
 
-        self.assertEqual(200, response.status_code)
+		with allure.step("Verify DEBUG flag"):
+			self.assertFalse(app.config['DEBUG'])
 
-        data = json.loads(response.get_data(as_text=True))
-        print("\nDATA:\n{}\n".format(data))  # Debug only
+	def test_put_cars_update(self):
+		"""
+		PUT method requests for the enclosed entity be stored under
+		the supplied Request-URI. If the Request-URI refers to an
+		already existing resource – an update operation will happen,
+		otherwise create operation should happen if Request-URI is a
+		valid resource URI (assuming client is allowed to determine
+		resource identifier).
+		:return:
+		"""
+		allure.dynamic.title("Update car properties using PUT call")
+		allure.dynamic.severity(allure.severity_level.NORMAL)
 
-        self.assertTrue(data['response']['successful'])
+		with allure.step("Prepare test data"):
+			car = {"name": "Creta",
+			       "brand": "Hyundai",
+			       "price_range": "8-14 lacs",
+			       "car_type": "hatchback"}
+
+			user = {"name": "eric",
+			        "password": "testqxf2",
+			        "perm": "non_admin"}
+
+		with allure.step("Send PUT request"):
+			response = app.test_client().put(
+				'{}{}'.format('/cars/update/',
+				              car['name']),
+				data=json.dumps(car),
+				content_type='application/json',
+
+				# Testing Flask application
+				# with basic authentication
+				# Source: https://gist.github.com/jarus/1160696
+				headers={
+					'Authorization': 'Basic ' +
+					                 base64.b64encode(bytes(user['name'] +
+					                                        ":" +
+					                                        user['password'],
+					                                        'ascii')).decode('ascii')
+				}
+			)
+
+		with allure.step("Verify status code"):
+			self.assertEqual(200, response.status_code)
+
+		with allure.step("Convert response into JSON data"):
+			data = json.loads(response.get_data(as_text=True))
+			print("\nDATA:\n{}\n".format(data))  # Debug only
+
+		with allure.step("Verify 'successful' flag"):
+			self.assertTrue(data['response']['successful'])
